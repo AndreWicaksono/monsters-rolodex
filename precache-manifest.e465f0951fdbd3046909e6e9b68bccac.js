12f248011bf43c0b1229a51d5b0b51a427705da0
AndreWicaksono <andrewicaksana@gmail.com>
2019-07-09 14:35:45 +0700

    Updates

---
+++ precache-manifest.e465f0951fdbd3046909e6e9b68bccac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647b3412e173ff2988453b57dd14f81e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a26fb304d088e88335f4",
+    "url": "/monsters-rolodex/static/css/main.440e3330.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a26fb304d088e88335f4",
+    "url": "/monsters-rolodex/static/js/main.23852391.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
